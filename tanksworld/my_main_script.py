ad63ce7f327fbb09152d906e5e78c8aba13fa8eb
rivercg1 <corban.rivera@jhuapl.edu>
2021-01-28 16:16:16 -0500

    fixed main script, and added conda environment.yml file to reproduce the environment

--- tanksworld/my_main_script.py
+++ tanksworld/my_main_script.py
@@ -18,13 +18,13 @@ arena = make_stem(cfg.MAKE_ENV_LOCATION, cfg.LOG_COMMS_DIR, cfg.OBS_SPACES, cfg.
 # --- only the root process will get beyond this point ---
 
 # the first 5 players in the gamew will use policy 1
-match_list = [[1,1,1,1,1,1,1,1,1,1]]
+match_list = [[1,1,1,1,1]]
 
 # policy 1 is PPO
 policy_types = {1:"ppo"}
 
 #kwargs to configure the environment
-kwargs_1 = {"static_tanks":[], "random_tanks":[], "disable_shooting":[],"friendly_fire":False}
+kwargs_1 = {"static_tanks":[], "random_tanks":[5,6,7,8,9], "disable_shooting":[],"friendly_fire":False}
 
 # run each copy of the environment for 300k steps
 arena.kickoff(match_list, policy_types, 300000, scale=True, render=False, env_kwargs=kwargs_1)
